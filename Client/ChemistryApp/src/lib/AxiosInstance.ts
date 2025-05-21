135839c8dda247a04648221e5856b405dd8ee5eb
Trần Nhựt Anh <137797291+NaGit09@users.noreply.github.com>
2025-05-21 19:25:41 +0700

    Update AxiosInstance.ts
    edit api url

--- Client/ChemistryApp/src/lib/AxiosInstance.ts
+++ Client/ChemistryApp/src/lib/AxiosInstance.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-    baseURL : "http://localhost:8080/"
+    baseURL : "http://18.141.164.54:8080/"
 })
 
 export default axiosInstance ;
